ff25def0d92a6601738153af32e2536964979888
Aftab <40713086+aftab36@users.noreply.github.com>
2018-07-03 00:22:17 +0300

    add task 1

---
+++ task1.php
@@ -0,0 +1,15 @@
+<?php
+$x = 16;
+$y = 5;
+ echo "$x * $y => ". $z = $x * $y . "<br/>" . "$x % $y => ". $m = $x % $y ."<br/>";
+
+if($m == 8)
+{echo $m . "this num is equal 8 " ."<br/>";}
+elseif ($m!= 8)
+{echo $m . "this num is not equal 8 " ."<br/>";}
+elseif ($m > 8)
+{echo $m . "this num is bigger 8 " ."<br/>";}
+else {echo "not equal";}
+
+
+?>
